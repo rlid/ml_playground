994b82747542cc9588fa8a40238b6ae62a13ccbd
Ryan Li <ryan@knowble.com>
2022-06-23 12:24:24 +0100

    added example using gradient descent to solve linear regression (4.2 of HPML, p36)

---
+++ linear_regression_with_gradient_descent.py
@@ -0,0 +1,35 @@
+import math
+import numpy as np
+
+
+def generate_data(w, b, n=1000):
+    xs = 5 * np.random.rand(n)
+    ys = w * xs + b + np.random.normal(0, 1, n)
+    return xs, ys
+
+
+def f_le(x, xs, ys, w, b, r=0.001):
+    n = len(xs)
+    dl_dw = -2 * np.matmul(np.transpose(xs), ys - (w * xs + b)) / n
+    dl_db = -2 * np.matmul(np.ones(n), ys - (w * xs + b)) / n
+    # Note: normalise L by sample size so the hyperparameters such as learning rate are not sensitive to sample size
+
+    w -= r * dl_dw
+    b -= r * dl_db
+
+    return w * x + b, w, b
+
+
+def avg_loss(xs, ys, w, b):
+    e = ys - (w * xs + b)
+    return math.sqrt(np.matmul(np.transpose(e), e)) / len(xs)
+
+
+data_x, data_y = generate_data(3, 2)
+e_w = 1
+e_b = 0
+n_run = 15000
+for i in range(n_run):
+    y, e_w, e_b = f_le(x=1, xs=data_x, ys=data_y, w=e_w, b=e_b)
+    if i % (n_run / 15) == 0:
+        print(i, y, avg_loss(data_x, data_y, e_w, e_b))
